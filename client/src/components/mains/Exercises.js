4915ebd599205182be22a1ed1293bfc2c7906d4e
dealhouse <de.juan1@live.com>
2022-05-14 23:26:46 -0400

    added update and delete functionality to exercises

--- client/src/components/mains/Exercises.js
+++ client/src/components/mains/Exercises.js
@@ -20,10 +20,58 @@ const Exercises = (props) => {
         props.fetchExercise()
     }, [clicked])
     const [clicked, setClicked] = useState(0)
+    const [updating, setUpdating] = useState()
+    const [formValues, setFormValues] = useState({
+        name: '',
+        type: '',
+        description: '',
+        repetitions: '',
+        sets: '',
+        weight: '',
+        plan: parseInt(id)
+    })
+    const handleChange = (e) => {
+        setFormValues({...formValues, [e.target.name]: e.target.value})
+    }
+    const handleSubmit = (e, id) => {
+        e.preventDefault()
+        props.updateExercise(id, formValues)
+        setFormValues(
+            {
+                name: '',
+                type: '',
+                description: '',
+                repetitions: '',
+                sets: '',
+                weight: '',
+                plan: parseInt(id)
+            })
+        setUpdating()
+        setClicked((prev) => prev + 1)
+    }
+    
+    const handleClickEd = (e, exercise) => {
+        e.preventDefault()
+        setUpdating(exercise.id)
+        setFormValues({
+                name: exercise.name,
+                type: exercise.type,
+                description: exercise.description,
+                repetitions: exercise.repetitions,
+                sets: exercise.sets,
+                weight: exercise.weight,
+                plan: parseInt(id)
+        })
+
+    }
+    const handleClickDel = (e, id) => {
+        e.preventDefault()
+        props.removeExercise(id)
+    }
     return (
         <div>
             <h2>Exercises</h2>
-            {/* <form onSubmit={(e) => handleSubmit(e, updating)}> */}
+            <form onSubmit={(e) => handleSubmit(e, updating)}>
             <table className='table table-striped'>
                 <thead>
                     <tr>
@@ -40,7 +88,7 @@ const Exercises = (props) => {
                 </thead>
                 <tbody>
                     {props.exerciseState.exercises.filter(exercise => exercise.plan === parseInt(id)).map((exercise) => (
-                        // updating !== plan.id ?
+                        updating !== exercise.id ?
                         <tr key={exercise.id}>
                             <td>{exercise.id}</td>
                             <td>{exercise.name}</td>
@@ -49,61 +97,100 @@ const Exercises = (props) => {
                             <td>{exercise.repetitions}</td>
                             <td>{exercise.sets}</td>
                             <td>{exercise.weight}</td>
-                            {/* <td><button onClick={(e) => handleClickEd(e, plan)} className='btn btn-primary btn sm'>Edit</button></td>
-                            <td><button onClick={(e) => handleClickDel(e, plan.id)} className='btn btn-danger btn sm'>Delete</button></td> */}
+                            <td><button onClick={(e) => handleClickEd(e, exercise)} className='btn btn-primary btn sm'>Edit</button></td>
+                            <td><button onClick={(e) => handleClickDel(e, exercise.id)} className='btn btn-danger btn sm'>Delete</button></td>
                         </tr> 
-                        // :  
-                        // <tr>
-                        //     <td>
-                        // <div className="form-group">
-                        //     <label>Name</label>
-                        //     <input
-                        //     className="form-control"
-                        //     type="text"
-                        //     name="name"
-                        //     onChange={handleChange}
-                        //     value={formValues.name}
-                        //     />
-                        // </div>
-                        // </td>
-                        // <td>
+                        :  
+                        <tr>
+                            <td>
+                        <div className="form-group">
+                            <label>Name</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="name"
+                            onChange={handleChange}
+                            value={formValues.name}
+                            />
+                        </div>
+                        </td>
+                        <td>
                             
-                        // <div className="form-group">
-                        //     <label>Type</label>
-                        //     <input
-                        //     className="form-control"
-                        //     type="text"
-                        //     name="type"
-                        //     onChange={handleChange}
-                        //     value={formValues.type}
-                        //     />
-                        // </div>
-                        // </td>
-                        // <td>
+                        <div className="form-group">
+                            <label>Type</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="type"
+                            onChange={handleChange}
+                            value={formValues.type}
+                            />
+                        </div>
+                        </td>
+                        <td>
 
-                        // <div className="form-group">
-                        //     <label>Duration</label>
-                        //     <input
-                        //     className="form-control"
-                        //     type="text"
-                        //     name="duration"
-                        //     onChange={handleChange}
-                        //     value={formValues.duration}
-                        //     />
-                        // </div>
-                        // </td>
-                        // <td>
-                        // <div className="form-group">
-                        //     <button type="submit" className="btn btn-primary">
-                        //     Submit
-                        //     </button>
-                        // </div>
-                        //     </td>
-                        // </tr>
+                        <div className="form-group">
+                            <label>Description</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="description"
+                            onChange={handleChange}
+                            value={formValues.description}
+                            />
+                        </div>
+                        </td>
+                        <td>
+
+                        <div className="form-group">
+                            <label>Repetitions</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="repetitions"
+                            onChange={handleChange}
+                            value={formValues.repetitions}
+                            />
+                        </div>
+                        </td>
+                        <td>
+
+                        <div className="form-group">
+                            <label>Sets</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="sets"
+                            onChange={handleChange}
+                            value={formValues.sets}
+                            />
+                        </div>
+                        </td>
+                        <td>
+
+                        <div className="form-group">
+                            <label>Weight</label>
+                            <input
+                            className="form-control"
+                            type="text"
+                            name="weight"
+                            onChange={handleChange}
+                            value={formValues.weight}
+                            />
+                        </div>
+                        </td>
+                        <td>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">
+                            Submit
+                            </button>
+                        </div>
+                            </td>
+                        </tr>
                     ))}
                 </tbody>
             </table>
-                    {/* </form> */}
+                    </form>
         </div>
     );
 }
